63851a7c71ef141498b957a2fa970bbfb37b5769
Headrick <104570437+headrickleonard@users.noreply.github.com>
2024-10-02 10:17:33 +0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://173.208.138.219:8003';
+export const BASE_URL = 'https://e25b-2001-4858-aaaa-95-42c8-5cff-feb5-391e.ngrok-free.app';
 // export const BASE_URL = 'http://192.168.1.172:8003';
 // export const BASE_URL = 'https://api.billingsystem.kibuti.co.tz';
